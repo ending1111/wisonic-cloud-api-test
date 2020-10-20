c4e84d96fdecaf9ed14525a505b4bed6789a8c4a
李沛林 <lipeilin@wisonic.cn>
2020-10-20 07:21:28 +0000

    Update debugtalk.py

--- debugtalk.py
+++ debugtalk.py
@@ -1,9 +1,10 @@
 # -*- coding: UTF-8 -*-
 from jsonschema import validate
 from jsonschema import ValidationError
+from httprunner.exceptions import ValidationFailure
 
 def json_schema(json, schema):
     try:
         validate(instance=json, schema=schema)
-    except ValidationError:
-        raise AssertionError("Bad json")
+    except ValidationError as e:
+        raise ValidationFailure("Bad json") from e
