a28ad494e12785bc03accd7e1b56b5d5ee8ac037
高玉娴 <gaoyuxian@wisonic.com>
2021-12-10 15:17:22 +0800

    更改读取数据的方式，由读取txt文件方式改成读取ini文件

--- pages/image_quality_page.py
+++ pages/image_quality_page.py
@@ -4,8 +4,21 @@
 
 from common.base_request import Baserequest
 from common.operate_txt_file import OperateTxtFile
+import configparser
+import os
 
 class ImageQualityPage(object):
+
+    root_dir = os.path.dirname(os.path.dirname(__file__))
+    print(root_dir)
+    file = root_dir + '/config/' + 'param_config.ini'
+    print(file)
+    config = configparser.ConfigParser()
+    config.read(file, encoding='UTF-8')
+    # 测试地址
+    test_env = config.get("env","test_env")
+    print(test_env)
+    request = Baserequest()
 
     def image_task_list(self,pageNumber,pageSize,months):
         """
@@ -15,12 +28,12 @@ class ImageQualityPage(object):
         :param months:
         :return:
         """
-        request = Baserequest()
-        txt_operate = OperateTxtFile()
-        test_env = txt_operate.read_row_keyword("config.txt","test_env")
+        # request = Baserequest()
+        # txt_operate = OperateTxtFile()
+        # test_env = txt_operate.read_row_keyword("config.txt","test_env")
         params = {"pageNumber":pageNumber,"pageSize":pageSize,"months":months}
-        url = test_env + "/api/uniqc/image-qc/task-list"
-        r = request.get_noNeedToken_params(url=url,params=params)
+        url = self.test_env + "/api/uniqc/image-qc/task-list"
+        r = self.request.get_noNeedToken_params(url=url,params=params)
         result = r.json()
         #print(result)
         return result
--- pages/login_page.py
+++ pages/login_page.py
@@ -3,9 +3,22 @@
 
 from common.base_request import Baserequest
 from common.operate_txt_file import OperateTxtFile
+import configparser
+import os
 
 
 class LoginPage(object):
+
+    root_dir = os.path.dirname(os.path.dirname(__file__))
+    print(root_dir)
+    file = root_dir + '/config/' + 'param_config.ini'
+    print(file)
+    config = configparser.ConfigParser()
+    config.read(file, encoding='UTF-8')
+    # 测试地址
+    test_env = config.get("env","test_env")
+    print(test_env)
+    request = Baserequest()
 
     def get_hospital_info(self,phone):
         """
@@ -13,13 +26,13 @@ class LoginPage(object):
         :param phone:
         :return:
         """
-        request = Baserequest()
-        txt_operate = OperateTxtFile()
-        test_env = txt_operate.read_row_keyword("config.txt", "test_env")
-        url = test_env + "/api/pacs/login/hospital"
+        # request = Baserequest()
+        # txt_operate = OperateTxtFile()
+        # test_env = txt_operate.read_row_keyword("config.txt", "test_env")
+        url = self.test_env + "/api/pacs/login/hospital"
         print(url)
         params = {"phone":phone}
-        r = request.get_noNeedToken_params(url=url,params=params)
+        r = self.request.get_noNeedToken_params(url=url,params=params)
         result = r.json()
         #print(result)
         return result
@@ -33,17 +46,17 @@ class LoginPage(object):
         :param departmentId:
         :return:
         """
-        request = Baserequest()
-        txt_operate = OperateTxtFile()
-        test_env = txt_operate.read_row_keyword("config.txt", "test_env")
-        url = test_env + "/api/pacs/login/password"
+        # request = Baserequest()
+        # txt_operate = OperateTxtFile()
+        # test_env = txt_operate.read_row_keyword("config.txt", "test_env")
+        url = self.test_env + "/api/pacs/login/password"
         data = {
             "phone": phone,
             "credentials": credentials,
             "hospitalId": hospitalId,
             "departmentId": departmentId
         }
-        r = request.post_noNeedToken(url, data)
+        r = self.request.post_noNeedToken(url, data)
         result = r.json()
         return result
 
@@ -56,17 +69,17 @@ class LoginPage(object):
         :param departmentId:
         :return:
         """
-        request = Baserequest()
-        txt_operate = OperateTxtFile()
-        test_env = txt_operate.read_row_keyword("config.txt", "test_env")
-        url = test_env + "/api/pacs/login/password"
+        # request = Baserequest()
+        # txt_operate = OperateTxtFile()
+        # test_env = txt_operate.read_row_keyword("config.txt", "test_env")
+        url = self.test_env + "/api/pacs/login/password"
         data = {
             "phone": phone,
             "credentials": credentials,
             "hospitalId": hospitalId,
             "departmentId": departmentId
         }
-        token = request.post_result_token(url, data)
+        token = self.request.post_result_token(url, data)
         return token
 
     def login_room(self,token):
@@ -75,11 +88,11 @@ class LoginPage(object):
         :param token:
         :return:
         """
-        request = Baserequest()
-        txt_operate = OperateTxtFile()
-        test_env = txt_operate.read_row_keyword("config.txt", "test_env")
-        url = test_env + "/api/pacs/login/room"
-        r = request.get(url,token)
+        # request = Baserequest()
+        # txt_operate = OperateTxtFile()
+        # test_env = txt_operate.read_row_keyword("config.txt", "test_env")
+        url = self.test_env + "/api/pacs/login/room"
+        r = self.request.get(url,token)
         result = r.json()
         return result
 
